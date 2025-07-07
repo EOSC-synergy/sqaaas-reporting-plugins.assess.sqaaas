4fdad1329c0addc0954919f0d031ffc86508f295
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2025-07-07 08:00:47 +0100

    Add JePL file structure

--- .sqa/script.gamy-myrtle-horse.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/eosc-synergy/sqaaas-reporting-plugins &&
-    jsonlint-cli "**/*.json"
-)
--- .sqa/script.muzzy-celadon-boar.sh
+++
@@ -1,4 +0,0 @@
-(
-cd github.com/eosc-synergy/sqaaas-reporting-plugins &&
-    licensee detect . --confidence 60 --json
-)
